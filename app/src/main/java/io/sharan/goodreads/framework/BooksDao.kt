085e9f7c6f9c3f3fdbb57737d716f7f415faee6c
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-09 15:51:29 +0530

    Add delete book dao

--- app/src/main/java/io/sharan/goodreads/framework/BooksDao.kt
+++ app/src/main/java/io/sharan/goodreads/framework/BooksDao.kt
@@ -2,6 +2,7 @@ package io.sharan.goodreads.framework
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -12,6 +13,9 @@ interface BooksDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(book: Book)
+
+    @Delete
+    suspend fun delete(book: Book)
 
     @Query("SELECT * from books")
     fun getBooks(): LiveData<List<Book>>
