b0d4140f22d23640a49c65f4377fecd564238941
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-11 12:06:05 +0530

    Update FakeBooksRepositoryImpl

--- app/src/test/java/io/sharan/goodreads/repositories/FakeBooksRepositoryImpl.kt
+++ app/src/test/java/io/sharan/goodreads/repositories/FakeBooksRepositoryImpl.kt
@@ -1,29 +1,57 @@
 package io.sharan.goodreads.repositories
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.sharan.goodreads.business.data.Book
 import io.sharan.goodreads.framework.data.remote.responses.ImageResponse
 import io.sharan.goodreads.framework.other.Resource
 import io.sharan.goodreads.framework.repositories.BooksRepository
 
 class FakeBooksRepositoryImpl : BooksRepository {
+
+    private val books = mutableListOf<Book>()
+
+    private val observableBooks = MutableLiveData<List<Book>>(books)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableBooks.postValue(books)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return books.sumOf { it.price.toDouble() }.toFloat()
+    }
+
     override suspend fun insertBook(book: Book) {
-        TODO("Not yet implemented")
+        books.add(book)
+        refreshLiveData()
     }
 
     override suspend fun deleteBook(book: Book) {
-        TODO("Not yet implemented")
+        books.remove(book)
+        refreshLiveData()
     }
 
     override fun observeAllBooks(): LiveData<List<Book>> {
-        TODO("Not yet implemented")
+        return observableBooks
     }
 
     override fun observeTotalPrice(): LiveData<Float> {
-        TODO("Not yet implemented")
+        return observableTotalPrice
     }
 
     override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
-        TODO("Not yet implemented")
+        return if (shouldReturnNetworkError) {
+            Resource.error("Error", null)
+        } else {
+            Resource.success(ImageResponse(emptyList(), 0, 0))
+        }
     }
 }
