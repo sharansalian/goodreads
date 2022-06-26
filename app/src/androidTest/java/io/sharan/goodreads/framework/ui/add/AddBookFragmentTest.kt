0052297ec4684ec857086e34e34f7a0d3e604420
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-26 19:49:00 +0530

    Test insert book into the db from btn click from fragment

--- app/src/androidTest/java/io/sharan/goodreads/framework/ui/add/AddBookFragmentTest.kt
+++ app/src/androidTest/java/io/sharan/goodreads/framework/ui/add/AddBookFragmentTest.kt
@@ -1,12 +1,15 @@
 package io.sharan.goodreads.framework.ui.add
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.sharan.goodreads.launchFragmentInHiltContainer
@@ -17,6 +20,12 @@ import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import io.sharan.goodreads.R
+import io.sharan.goodreads.business.data.Book
+import io.sharan.goodreads.framework.ui.BookFragmentFactory
+import io.sharan.goodreads.framework.ui.BooksViewModel
+import io.sharan.goodreads.getOrAwaitValue
+import io.sharan.goodreads.repositories.FakeBooksRepositoryImpl
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -25,6 +34,12 @@ class AddBookFragmentTest {
 
     @get: Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get: Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: BookFragmentFactory
 
     @Before
     fun setup() {
@@ -35,7 +50,7 @@ class AddBookFragmentTest {
     fun pressBackButton_popBackStack() {
         val navController = mock(NavController::class.java)
 
-        launchFragmentInHiltContainer<AddBookFragment> {
+        launchFragmentInHiltContainer<AddBookFragment>(fragmentFactory = fragmentFactory) {
             Navigation.setViewNavController(requireView(), navController)
         }
 
@@ -47,7 +62,7 @@ class AddBookFragmentTest {
     @Test
     fun clickBookImage_navigateToImageFragment() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<AddBookFragment> {
+        launchFragmentInHiltContainer<AddBookFragment>(fragmentFactory = fragmentFactory) {
             Navigation.setViewNavController(requireView(), navController)
         }
 
@@ -55,6 +70,29 @@ class AddBookFragmentTest {
 
         verify(navController).navigate(
             AddBookFragmentDirections.actionAddBookFragmentToImageFragment()
+        )
+    }
+
+    @Test
+    fun clickInsertIntoDb_bookInsertedIntoDb() {
+        val testViewModel = BooksViewModel(FakeBooksRepositoryImpl())
+        launchFragmentInHiltContainer<AddBookFragment>(fragmentFactory = fragmentFactory) {
+            booksViewModel = testViewModel
+        }
+
+        onView(withId(R.id.etbookName)).perform(replaceText("Book"))
+        onView(withId(R.id.etBookAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etBookPrice)).perform(replaceText("5.5"))
+
+        onView(withId(R.id.btnAddBookItem)).perform(click())
+
+        assertThat(testViewModel.books.getOrAwaitValue()).contains(
+            Book(
+                title = "Book",
+                author = "JK",
+                price = 5.5f,
+                amount = 5,
+            )
         )
     }
 }
--- app/src/main/java/io/sharan/goodreads/framework/ui/add/AddBookFragment.kt
+++ app/src/main/java/io/sharan/goodreads/framework/ui/add/AddBookFragment.kt
@@ -6,18 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import io.sharan.goodreads.business.data.Book
 import io.sharan.goodreads.databinding.FragmentAddBookBinding
 import io.sharan.goodreads.framework.ui.BooksViewModel
 import io.sharan.goodreads.framework.handleBackPress
+import io.sharan.goodreads.framework.other.Status
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AddBookFragment @Inject constructor(private val glide: RequestManager) : Fragment() {
 
-    val booksViewModel: BooksViewModel by viewModels()
+    lateinit var booksViewModel: BooksViewModel
 
     lateinit var binding: FragmentAddBookBinding
 
@@ -32,14 +36,51 @@ class AddBookFragment @Inject constructor(private val glide: RequestManager) : F
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        booksViewModel = ViewModelProvider(requireActivity())[BooksViewModel::class.java]
 
         binding.ivBookImage.setOnClickListener {
             findNavController().navigate(AddBookFragmentDirections.actionAddBookFragmentToImageFragment())
         }
 
-        //Find extension in UEFA
         handleBackPress {
             booksViewModel.setCurImageUrl("")
+        }
+
+        binding.btnAddBookItem.setOnClickListener {
+            booksViewModel.validateBook(
+                name = binding.etbookName.text.toString(),
+                amountString = binding.etBookAmount.text.toString(),
+                priceString = binding.etBookPrice.text.toString()
+            )
+        }
+    }
+
+    private fun subscribeToObservers() {
+        booksViewModel.curImageUrl.observe(viewLifecycleOwner) {
+            glide.load(it).into(binding.ivBookImage)
+        }
+
+        booksViewModel.insertBookStatus.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireView(),
+                            "Add Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireView(),
+                            result.message ?: "Unknown error occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.LOADING -> Unit
+                }
+            }
         }
     }
 }
