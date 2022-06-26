34c8e030bf0f5ec826b6acfdf03a0b8a76b3e7b2
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-26 15:34:19 +0530

    Add BaseFragmentViewBinding.kt impl

--- app/src/main/java/io/sharan/goodreads/framework/ui/books/BooksFragment.kt
+++ app/src/main/java/io/sharan/goodreads/framework/ui/books/BooksFragment.kt
@@ -1,33 +1,20 @@
 package io.sharan.goodreads.framework.ui.books
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.sharan.goodreads.databinding.FragmentBooksBinding
 import io.sharan.goodreads.framework.BookListener
 import io.sharan.goodreads.framework.BooksAdapter
+import io.sharan.goodreads.framework.common.BaseFragmentViewBinding
 import io.sharan.goodreads.framework.ui.BooksViewModel
 
 @AndroidEntryPoint
-class BooksFragment : Fragment() {
+class BooksFragment : BaseFragmentViewBinding<FragmentBooksBinding>(FragmentBooksBinding::inflate) {
 
     val booksViewModel: BooksViewModel by viewModels()
-
-    lateinit var binding: FragmentBooksBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentBooksBinding.inflate(layoutInflater)
-        return binding.root
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
